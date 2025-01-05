8c708b3387876ba4f82c10e4334fa530544fa153
K A I R A V <108761467+IlhamKurniawanBlora@users.noreply.github.com>
2025-01-05 17:19:05 +0700

    Update AsyncKotaView.vue

--- src/components/AsyncKotaView.vue
+++ src/components/AsyncKotaView.vue
@@ -128,7 +128,7 @@ const route = useRoute();
 const getDataCuaca = async () => {
     try {
         const DataCuaca = await axios.get(
-            `https://api.openweathermap.org/data/2.5/onecall?lat=${route.query.lat}&lon=${route.query.lng}&exclude={part}&appid=86ef87ad2764e0f33b4034b66a2340a9&units=imperial`
+            `https://api.openweathermap.org/data/2.5/onecall?lat=${route.query.lat}&lon=${route.query.lng}&exclude={part}&appid=416f0ed0bb28d3110beedecf5fa9cf85&units=imperial`
         );
 
         // menghitung hari dan waktu
